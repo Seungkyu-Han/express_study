5ac6e5201208dca85b27311730f96e4120288ff0
Seungkyu-Han <trust1204@gmail.com>
2026-06-27 08:31:00 +0900

    feat: add file upload

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,32 @@ import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'node:path';
+import multer from 'multer';
+import fs from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 dotenv.config();
 
+const uploadDir = path.join(__dirname, 'public/uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, uploadDir);
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, `${Date.now()}${ext}`);
+        },
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    },
+});
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -37,5 +57,10 @@ app.use((req, res, next) => {
     console.log('미들웨어 지나갑니다.');
     next();
 });
+
+app.post("/file", upload.single("file"), (req, res) => {
+    console.log(req.file);
+    res.send(req.file);
+})
 
 app.listen(app.get('port'), () => console.log('Server on'));
